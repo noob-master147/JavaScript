fa31d8500a2980e55c2cb8569308a81ad7fda5ef
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-05-24 22:53:03 +0530

    boolandnull

---
+++ 01Basics/nullandBool.js
@@ -0,0 +1,24 @@
+//temp is coming from website API
+
+var temp
+
+//done some calculations
+
+// temp = 2
+
+// console.log("current temperature is " + temp)
+
+//Grading system of school
+
+//10 - Great job
+//5 - work harder
+//2 - poor
+//0 - fail
+
+//Boolean operation - True/False
+
+let actualMarks = 7
+
+let grade = (actualMarks < 10)
+
+console.log(grade)
