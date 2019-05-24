56fdd54f783e119666169f3b1bf76f882c87f55a
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-05-24 23:08:10 +0530

    if and else

---
+++ 02Basics/learnIfElse.js
@@ -0,0 +1,28 @@
+// if(false){
+//     console.log('I am inside if Block')
+// }
+
+// else{
+//     console.log('i am inside else Block')
+// }
+
+
+var whoIsHere = 'user'
+
+if(whoIsHere === 'user'){
+    console.log('Greating message for user')
+    console.log('Allow acces to view all courses')
+
+}
+
+else if(whoIsHere === 'teacher'){
+    console.log('Greating message for teacher')
+    console.log('Allow acces to view his courses')
+
+}
+
+else{
+    console.log('MESSAGE: Please verify yourself')
+    console.log('semd user an email')
+ 
+}
