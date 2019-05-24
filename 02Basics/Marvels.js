c101d0629f29168f3dd49c0e7113ffe136d60365
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-05-25 00:21:36 +0530

    tilldays

---
+++ 02Basics/Marvels.js
@@ -0,0 +1,14 @@
+//datatype - Array
+
+const superHeroes = ['Thor','IronMan','SpiderMan','capt.America']
+
+//display all the values
+console.log(superHeroes);
+
+//display specific value
+console.log(superHeroes[0]);
+
+//display last value
+console.log(superHeroes[superHeroes.length-1])
+
+console.log(`we have ${superHeroes.length} super heroes`)
