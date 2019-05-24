8138cfecb82510e995be8e29fa7f7ec02236788d
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-05-24 22:42:19 +0530

    celsius

---
+++ 01Basics/celsius.js
@@ -0,0 +1,18 @@
+var score = 100
+
+score = 130
+
+const bonus = 50
+
+var finalScore = (score + bonus) * 1.8
+
+var complex = 4 + 4 * 5 / 2
+
+// console.log(finalScore)
+// console.log(complex)
+
+let fahrenheit = 100
+
+let celsius = (fahrenheit-32) / (9/5)
+
+console.log(celsius)
