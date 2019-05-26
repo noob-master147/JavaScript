e91f178b6f40893fb03f50a65c2b05ba2f5d3ffd
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-05-26 19:10:31 +0530

    arrow function

---
+++ 01Advance/arrow.js
@@ -0,0 +1,22 @@
+// let sayHello = function(name)
+// {
+//   return 'Hey there, ' + name + ' !'
+// }
+
+// console.log(sayHello('John Doe'))
+
+
+// let sayHello = (name) =>
+// {
+//   return 'Hey there, ' + name + ' !'
+// }
+
+// console.log(sayHello('John Doe'))
+
+// // this same function above can be 
+// // written in a different way and a shorter way too!!!!!
+
+
+let satHello = (name) => `Hey ${name}. Welcome to my Store!`
+
+console.log(satHello('John Doe'))
