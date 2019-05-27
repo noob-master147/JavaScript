9459032c642ab8095c07c32f0c78daff43696fe7
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-05-27 14:21:12 +0530

    arrow function

--- 01Advance/arrow.js
+++ 01Advance/arrow.js
@@ -17,6 +17,67 @@
 // // written in a different way and a shorter way too!!!!!
 
 
-let satHello = (name) => `Hey ${name}. Welcome to my Store!`
+// let satHello = (name) => `Hey ${name}. Welcome to my Store!`
 
-console.log(satHello('John Doe'))
+// console.log(satHello('John Doe'))
+
+
+// const todos = [
+//     {
+//         title: 'Buy Bread',
+//         isDone: true,
+//     },
+//     {
+//         title: 'Go to Gym',
+//         isDone: true,
+//     },
+//     {
+//         title: 'Record YT Videos',
+//         isDone: false,
+//     },
+// ]
+
+// //function to pickup true todos
+
+// let thingsDone = todos.filter((todo) => todo.isDone === true)
+// console.log(thingsDone)
+
+
+// Assignment 
+// create a todo with 6 items 
+// print the titles of the todos which are not done
+
+const task =[
+    {
+        title: 'one', 
+        status: false,
+    },
+    {
+        title: 'two',
+        status: true,
+    },
+    {
+        title: 'three',
+        status: true,
+    },
+    {
+        title: 'four',
+        status: false,
+    },
+    {
+        title: 'five',
+        status: false,
+    },
+    {
+        title: 'six',
+        status: false,
+    }
+]
+
+let notDone = (task.filter((todo) => !todo.status))
+notDone.forEach((task) => console.log(task.title))
+
+
+// Alternative
+// const thingsDone = task.filter((todo) => todo.status === false)
+// thingsDone.forEach((task) => console.log(task.title))
